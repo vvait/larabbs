a0abf2ee82bd34ea7d826ad7d79efb3aff1f96c0
liyy <yvonngj@163.com>
2019-03-26 09:20:32 +0000

    user can register using phone

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -19,21 +19,23 @@ class VerificationCodesController extends Controller
     {
         $phone = $request->phone;
 
-        if (!app()->environment('production')) {
-            $code = '1234';
-        } else {
-            // 生成4位随机数，左侧补0
-            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+//        if (!app()->environment('production')) {
+//            $code = '1234';
+//        } else {
+//            // 生成4位随机数，左侧补0
+//            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+//
+//            try {
+//                $result = $easySms->send($phone, [
+//                    'content'  =>  "【李永羽】您的验证码是{$code}。如非本人操作，请忽略本短信"
+//                ]);
+//            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+//                $message = $exception->getException('yunpian')->getMessage();
+//                return $this->response->errorInternal($message ?: '短信发送异常');
+//            }
+//        }
 
-            try {
-                $result = $easySms->send($phone, [
-                    'content'  =>  "【李永羽】您的验证码是{$code}。如非本人操作，请忽略本短信"
-                ]);
-            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-                $message = $exception->getException('yunpian')->getMessage();
-                return $this->response->errorInternal($message ?: '短信发送异常');
-            }
-        }
+        $code = '1234';
 
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
