b2cb5fb798e3ce0bb8722ebab356b57d04b287b4
liyy <yvonngj@163.com>
2019-03-29 07:20:45 +0000

    add socialtiteproviders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,12 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
+//        'App\Events\Event' => [
+//            'App\Listeners\EventListener',
+//        ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
         ],
     ];
 
@@ -26,6 +30,18 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
+
+//        $accessToken = '20_955ZmpePX4Vj1273nauLjMezxzlR5Wg7NyN5_wgSf3qUwv7nDHFBCRJQ7L2g6bfKP4nww0O_uiIXoMkgNLRG2Q';
+//        $openID = 'oI8G41MFdnqUcznapo-p08v8aQg0';
+//        $driver = Socialite::driver('weixin');
+//        $driver->setOpenId($openID);
+//        $oauthUser = $driver->userFromToken($accessToken);
+
+//        $code = '061pSxwf0alGbu1S9huf0WyMwf0pSxwF';
+//        $driver = Socialite::driver('weixin');
+//        $response = $driver->getAccessTokenResponse($code);
+//        $driver->setOpenId($response['openid']);
+//        $oauthUser = $driver->userFromToken($response['access_token']);
 
         //
     }
