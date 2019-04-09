a138496342a79a1813f381de1a4ecb97fcabb082
liyy <yvonngj@163.com>
2019-04-09 07:33:50 +0000

    topic delete api

--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,8 @@ $api->version('v1',[
             //修改话题
             $api->patch('topics/{topic}', 'TopicsController@update')
                 ->name('api.topics.update');
+            //删除话题
+            $api->delete('topics/{topic}','TopicsController@destroy')->name('api.topic.destroy');
 
         });
 
